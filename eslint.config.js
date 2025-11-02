603382e671ee605a590a4d55557e06daac15ad2c
AlkaidSTART <2595006848@qq.com>
2025-11-02 21:27:01 +0800

    5

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,17 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import pluginVue from 'eslint-plugin-vue'
+import vue from 'eslint-plugin-vue'
 
 export default [
   // 基础配置
   js.configs.recommended,
 
-  // Vue 配置
-  {
-    files: ['**/*.vue'],
-    ...pluginVue.configs['flat/recommended'],
-    languageOptions: {
-      globals: {
-        ...globals.browser
-      }
-    },
-    rules: {
-      // 自定义规则
-      'vue/multi-word-component-names': 'off',
-      'vue/no-unused-components': 'warn',
-      'no-console': 'off',
-      'no-debugger': 'off'
-    }
-  },
+  // Vue配置
+  ...vue.configs['flat/recommended'],
 
-  // JavaScript 配置
+  // 自定义规则
   {
-    files: ['**/*.{js,mjs,cjs}'],
+    files: ['**/*.{js,vue,ts}'],
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -38,7 +23,8 @@ export default [
     rules: {
       'no-unused-vars': 'warn',
       'no-console': 'off',
-      'no-debugger': 'off'
+      'no-debugger': 'off',
+      'vue/multi-word-component-names': 'off'
     }
   },
 

18337871311d8bca76e197e86aa3b58fcfd19f24
AlkaidSTART <2595006848@qq.com>
2025-11-03 14:36:51 +0800

    13

--- src/api/article.js
+++
@@ -1,7 +0,0 @@
-import request from '@/utils/request'
-export function getArticleListService(params) {
-  return request.get('/my/article/list', { params })
-}
-export function getArticleDetailService(id) {
-  return request.get('/my/article/detail', { params: { id } })
-}
